b090862837dd24f975f5aec6b0ed201784459de6
Shuka Takakuma <shuka.takakuma@gmail.com>
2017-02-09 03:12:43 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Hamimoji Maker Bot
---
+++ src/main/java/sejour/linebot/hmb/HamimojiMakerBotApplication.java
@@ -0,0 +1,13 @@
+package sejour.linebot.hmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamimojiMakerBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HamimojiMakerBotApplication.class, args);
+    }
+
+}
