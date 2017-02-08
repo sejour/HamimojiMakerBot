055bb583b145279ac8524f729db79189c671ea73
Shuka Takakuma <shuka.takakuma@gmail.com>
2017-02-09 08:19:18 +0900

    add descriptions

--- src/main/java/sejour/linebot/hmb/hamimoji/HamimojiWriter.java
+++ src/main/java/sejour/linebot/hmb/hamimoji/HamimojiWriter.java
@@ -18,10 +18,23 @@ public class HamimojiWriter {
         this.hamimojiMaker = hamimojiMaker;
     }
 
+    /**
+     * テキストからはみ文字を生成し、ストリームに書き出す
+     * @param text テキスト
+     * @param out 出力ストリーム
+     * @throws Exception
+     */
     public void write(String text, OutputStream out) throws Exception {
         write(text, out, 0);
     }
 
+    /**
+     * テキストからはみ文字を生成し、ストリームに書き出す
+     * @param text テキスト
+     * @param out 出力ストリーム
+     * @param columnNumbers 最大列文字数（入力文字数が最大列文字数を超える場合は改行される。）
+     * @throws Exception
+     */
     public void write(String text, OutputStream out, int columnNumbers) throws Exception {
         Iterator<ImageWriter> imageWriters = ImageIO.getImageWritersByFormatName("gif");
         if (!imageWriters.hasNext()) throw new Exception("No image writers.");
